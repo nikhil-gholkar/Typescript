b188a7519721301d5ea22da78c07d332caab1d2a
Nikhil Gholakar <nikhil.gholkar76@gmail.com>
2026-05-03 19:31:47 +0530

    typescript types

---
+++ Basic types/typescriptTypes.ts
@@ -0,0 +1,10 @@
+
+
+const heading = document.querySelector('h1')
+console.log(heading?.textContent)
+// ? here means optional chaining so if heading is not null then
+// only we will get the textcontent
+
+// In TypeScript, every value is associated with a type.
+// A type is a label that describes the properties and methods that a value has.
+// TypeScript compiler uses types to analyze your code for hunting bugs and errors.
