f925e79b7fb031efefb72a30a26410d96aeac9fa
Nikhil Gholakar <nikhil.gholkar76@gmail.com>
2026-05-04 23:33:09 +0530

    tuppleType

---
+++ Basic types/tuppleType.ts
@@ -0,0 +1,9 @@
+
+let tup:[number,string]=[5,"hi"]
+console.log(tup)
+
+
+let tup2:[number, string, boolean?]=[15,"nik"]
+console.log(tup2)
+// boolean? here ? means optional 
+// A tuple is an array with a fixed number of elements whose types are known.
